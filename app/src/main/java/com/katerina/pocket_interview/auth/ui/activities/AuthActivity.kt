785cdd49cd600467b2f43bd2a32c9dbabe4b5a93
Katerina Morozova <kattrinmr@gmail.com>
2022-09-28 17:08:16 +0300

    add screens

---
+++ app/src/main/java/com/katerina/pocket_interview/auth/ui/activities/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.katerina.pocket_interview.auth.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.katerina.pocket_interview.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
