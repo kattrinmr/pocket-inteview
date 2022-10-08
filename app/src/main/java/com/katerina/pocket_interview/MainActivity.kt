284393a8daf2d1376a6700172a2453aabfade8bb
Katerina Morozova <kattrinmr@gmail.com>
2022-10-08 17:44:05 +0300

    add navigation

--- app/src/main/java/com/katerina/pocket_interview/MainActivity.kt
+++ app/src/main/java/com/katerina/pocket_interview/MainActivity.kt
@@ -2,10 +2,22 @@ package com.katerina.pocket_interview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.katerina.pocket_interview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+
     }
 }
